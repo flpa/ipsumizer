98b6b4811a8fdf83e8cdabb479727d9b488d2469
flpa <flpa.dev@zoho.com>
2014-03-12 22:28:28 +0100

    try to handle umlauts by wrapping stdin in ut8 codec

--- lorem-ipsumizer.py
+++ lorem-ipsumizer.py
@@ -2,11 +2,14 @@
 
 from collections import deque
 import sys
+import codecs
 
+lorems = deque()
+for char in open("lorem.txt").read():
+    if char.isalpha():
+        lorems.append(char)
 
-lorems = deque(open("lorem.txt").read().replace(' ', ''))
-
-for char in sys.stdin.read():
+for char in codecs.getreader("utf-8")(sys.stdin).read():
     if char.isalpha():
         loremChar = lorems.popleft()
         lorems.append(loremChar)
