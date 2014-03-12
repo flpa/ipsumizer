c6530b32a9188f73ef58e460d9f26e9fc9c27822
flpa <flpa.dev@zoho.com>
2014-03-12 22:06:19 +0100

    experiments in reading from stin writing to stdout, make executable

--- lorem-ipsumizer.py
+++ lorem-ipsumizer.py
@@ -1,6 +1,17 @@
+#! /usr/bin/python
+
 from collections import deque
+import sys
 
-dek = deque(open("lorem.txt").read().replace(' ', ''))
 
-for char in dek:
-    print "'"+char+"'"
+for char in sys.stdin.read():
+    sys.stdout.write(char)
+
+# dek = deque(open("lorem.txt").read().replace(' ', ''))
+
+# for char in dek:
+#     print "'"+char+"'"
+
+
+
+
